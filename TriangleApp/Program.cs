a7ba9f0feb092b2e25db52947194a2f976e55615
Srikanth246414 <chilukuris921@gmail.com>
2025-02-07 22:10:20 -0500

    Initial commit

---
+++ TriangleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿                //NAME:CILUKURI SRIKANTH(mutant version)
+
+namespace TriangleApp{
+public class Triangle
+{
+    public static bool IsIsosceles(int a, int b, int c)
+    {
+        //checking equilateral triangle instead of isosceles for mutation
+        return a == b && b == c ;
+    }
+    public static void Main(string[] args){
+        Console.WriteLine("TestExecuted by Chilukuri Srikanth");
+        Console.WriteLine(IsIsosceles(5,4,5));
+    }
+}
+}
+
---
+++ TriangleTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿        
+        // NAME : CLILUKURI SRIKANTH
+using NUnit.Framework;
+using TriangleApp;
+namespace TriangleTests{
+    [TestFixture]
+public class TriangleTests
+{
+    [Test]
+    public void TestIsosceles()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Srikanth");
+        Assert.That(Triangle.IsIsosceles(4, 5, 4),Is.True);  
+
+    }
+     [Test]
+    public void Test_NoteIsosceles()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Srikanth");
+        Assert.That(Triangle.IsIsosceles(4, 5,6),Is.False);  
+    }
+     [Test]
+    public void TestEquilateral()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Srikanth");
+        Assert.That(Triangle.IsIsosceles(4, 4, 4),Is.True);  
+    }
+}
+}
